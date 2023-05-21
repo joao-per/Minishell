76a4934f8a125a5a98ad718a16118b794cdc33b9
Pedro Manuel Pereira Coelho Bigote <pedperei@c2r8s7.42lisboa.com>
2023-05-21 19:25:22 +0100

    treat expansion2

--- expansion.c
+++ expansion.c
@@ -6,7 +6,7 @@
 /*   By: pedperei <pedperei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/20 20:16:42 by pedperei          #+#    #+#             */
-/*   Updated: 2023/05/21 18:57:41 by pedperei         ###   ########.fr       */
+/*   Updated: 2023/05/21 19:24:55 by pedperei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,23 +20,23 @@ char *expansion(char *input, char *str_exp, t_env **env, int *i)
 	char *search;
 	t_env *env_var;
 
-	start = *i;
 	(*i)++;
+	start = *i;
 	while (ft_isalnum(input[*i]) || input[*i] == '_')
 		(*i)++;
 	end = *i;
 	search = ft_substr(input, start, end - start);
 	env_var = search_env_name(env, search);
 	free(search);
-	end = ft_strlen(input) - (end - start + 1) + ft_strlen(env_var->env_name);
+	end = ft_strlen(input) - (end - start + 2) + ft_strlen(env_var->env_value);
 	search = ft_calloc(end + 1, sizeof(char));
 	start = -1;
 	while (str_exp[++start] != '$')
 		search[start] = str_exp[start];
 	end = -1;
-	while (env_var->env_name[++end])
+	while (env_var->env_value[++end])
 	{
-		search[start] = env_var->env_name[end];
+		search[start] = env_var->env_value[end];
 		start++;
 	}
 	free(str_exp);
@@ -68,25 +68,31 @@ char	*treat_expansion(char *input, t_env **env)
 	int		*flag_type;
 	char	*str_exp;
 
-	quotes_open = 0;
-	flag_type = 0;
+	quotes_open = ft_calloc(1, sizeof(int));
+	flag_type = ft_calloc(1, sizeof(int));
 	str_exp = ft_calloc(ft_strlen(input) + 1, sizeof(char));
 	i = ft_calloc(1, sizeof(int));
 	while (input[*i])
 	{
 		exp_quote_update(input[*i], flag_type, quotes_open);
 		if (input[*i] == '$' && *flag_type != 1)
+		{
+			str_exp[*i] = input[*i];
 			str_exp = expansion(input, str_exp, env, i);
+		}
 		else
 		{
 			str_exp[*i] = input[*i];
 			(*i)++;
 		}
 	}
+	free(i);
+	free(quotes_open);
+	free(flag_type);
 	return (str_exp);
 }
 
-/* int	main(int ac, char **av, char **env)
+int	main(int ac, char **av, char **env)
 {
 	char *line = readline(">");
 	(void)ac;
@@ -103,4 +109,3 @@ char	*treat_expansion(char *input, t_env **env)
 
 	
 }  
- */
