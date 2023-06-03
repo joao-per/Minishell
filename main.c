efa790ab410ea775649c9fc6fc53c5965baace8b
Pedro Manuel Pereira Coelho Bigote <pedperei@c2r3s7.42lisboa.com>
2023-06-03 19:13:57 +0100

    norm

--- main.c
+++ main.c
@@ -76,6 +76,31 @@ void	cleanup_after_command(t_shell **shell)
 	}
 }
 
+int should_run_aux(t_shell *shell)
+{
+	int	should_run;
+
+	should_run = check_command(shell);
+	if (should_run == 1)
+	{
+		execute_command(shell);
+		cleanup_after_command(&shell);
+		return (1);
+	}
+	else if (should_run == -1)
+	{
+		free_env(shell);
+		cleanup_after_command(&shell);
+		return (-1);
+	}
+	else if (should_run == 2)
+	{
+		cleanup_after_command(&shell);
+		return (2);
+	}
+	return (0);
+}
+
 void	main_loop(t_env **env_vars, char **envs)
 {
 	int should_run;
@@ -97,23 +122,11 @@ void	main_loop(t_env **env_vars, char **envs)
 		shell = init_shell(env_vars, args, envs);
 		if (!shell)
 			continue ;
-		should_run = check_command(shell);
-		if (should_run == 1)
-		{
-			execute_command(shell);
-			cleanup_after_command(&shell);
-		}
-		else if (should_run == -1)
-		{
-			free_env(shell);
-			cleanup_after_command(&shell);
-			break ;
-		}
-		else if (should_run == 2)
-		{
-			cleanup_after_command(&shell);
+		should_run = should_run_aux(shell);
+		if (should_run == 2)
 			continue ;
-		}
+		if (should_run == -1)
+			break ;
 	}
 }
 
