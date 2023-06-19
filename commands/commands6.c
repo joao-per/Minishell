632830b368709d26126ba42292ad58d59be5e4ae
Pedro Manuel Pereira Coelho Bigote <pedperei@c2r6s2.42lisboa.com>
2023-06-19 20:44:37 +0100

    fix

--- commands/commands6.c
+++ commands/commands6.c
@@ -6,7 +6,7 @@
 /*   By: pedperei <pedperei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/03 15:56:23 by pedperei          #+#    #+#             */
-/*   Updated: 2023/06/19 17:15:53 by pedperei         ###   ########.fr       */
+/*   Updated: 2023/06/19 20:42:00 by pedperei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,11 @@ int	check_commands2(t_shell *shell, pid_t pid)
 		if (!handle_env(shell))
 			return (0);
 	}
+	if (pid != 0)
+	{
+		if (!handle_export(shell))
+			return (0);
+	}
 	if (pid != 0 && find_pipe_arg(shell, 0) == -1)
 	{
 		if (!handle_cd(shell))
@@ -55,22 +60,14 @@ int	check_commands2(t_shell *shell, pid_t pid)
 
 int	check_commands(t_shell *shell, pid_t pid)
 {
-	if (!shell->args_execve[0])
-		return (1);
 	if (pid != 0)
 	{
 		if (!handle_export(shell))
 			return (0);
 	}
-	if (pid != 0)
-	{
-		if (!handle_pwd(shell))
-			return (0);
-	}
-	if (pid != 0)
-	{
-		if (!handle_echo(shell))
-			return (0);
-	}
+	if (!handle_pwd(shell))
+		return (0);
+	if (!handle_echo(shell))
+		return (0);
 	return (check_commands2(shell, pid));
 }
